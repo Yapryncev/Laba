7161d21f4a32cb340ccd12ca89547cb676f74ba5
17001122 <Yapryntsev.E.O@sberbank.ru>
2019-04-22 17:11:15 +0300

    Исправлены ошибки

--- serv.py
+++ serv.py
@@ -2,26 +2,34 @@ import socket
 import ch_und as und
 
 sock = socket.socket()
-sock.bind(('127.0.0.1', 55556)) # ожидаем клиента
+sock.bind(('', 55556)) # ожидаем клиента
 sock.listen(1)
-
 while True:
     conn, addr = sock.accept()
-
-#print 'connected:', addr
-
+    print ('connected:', addr)
     while True:
-        if conn == 'PROCESS':
+        data = conn.recv(1024)
+        a = str(data, 'utf-8')
+        kd = 'PROCESS\n'
+        if a == kd:
             FH = und.Fishimage(None, None, None, None)
-            FH.understortV()
-            FH.understortN()
+            try:
+                FH.understortV()
+                FH.understortN()
+            except:
+                conn.send(b"ERR\n")
             FH.warpV()
             FH.warpN()
             FH.stitch()
-            conn.send('OK')   # команда клиенту
+            conn.send(b"OK\n")
+        else:
+            conn.send(b"ERR\n")
+        if not data:
+                break
+
+            #print (addr)# команда клиенту
         #data = conn.recv(1024) # нужно получить строго нужную длинну посылки
-        if not conn:
-            break
+
 
 
 conn.close()
